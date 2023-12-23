e79625cdbec58bcd0dd6c8a428168f6dfa25749d
MoriartyJam <jamesmoriartytrading2018@gmail.com>
2023-12-23 19:01:35 +0200

    Registration and login

---
+++ converter/registration/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import user_signup, user_login
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('login/', views.user_login, name='login'),
+    path('signup/', views.user_signup, name='signup'),
+    path('logout/', views.user_logout, name='logout'),
+]
---
+++ converter/registration/views.py
@@ -0,0 +1,53 @@
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import SignupForm, LoginForm
+from django.contrib import messages
+
+
+# Create your views here.
+# Home page
+def index(request):
+    return render(request, 'index.html')
+
+# signup page
+def user_signup(request):
+    if request.method == 'GET':
+        form = SignupForm()
+        return render(request, 'registration/signup.html', {'form': form})
+
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            messages.success(request, 'You have singed up successfully.')
+            login(request, user)
+            return HttpResponseRedirect("/")
+        else:
+            return render(request, 'registration/signup.html', {'form': form})
+
+# login page
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponseRedirect("/")
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+    return render(request, 'registration/login.html', {'form': form})
+
+# logout page
+def user_logout(request):
+    logout(request)
+    return redirect('login')
